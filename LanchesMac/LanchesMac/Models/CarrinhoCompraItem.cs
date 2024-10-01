fc27043f7d9b17b254b03cb1452e3847884333f1
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-10-01 15:32:14 -0300

    ✨ feat: defini as propriedades da classe CarrinhoCompraItem

--- LanchesMac/LanchesMac/Models/CarrinhoCompraItem.cs
+++ LanchesMac/LanchesMac/Models/CarrinhoCompraItem.cs
@@ -2,5 +2,9 @@
 {
     public class CarrinhoCompraItem
     {
+        public int CarrinhoCompraId { get; set; }
+        public Lanche Lanche { get; set; }
+        public int Quantidade { get; set; }
+        public string CarrinhCompra { get; set; }
     }
 }
