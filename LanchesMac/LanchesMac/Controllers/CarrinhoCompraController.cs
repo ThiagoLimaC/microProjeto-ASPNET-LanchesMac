c9067dd835ce3e39f15ed1c7c39919a417fa5533
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-10-14 13:46:25 -0300

    ✨ feat: defini as propriedades da ViewModel CarrinhoCompra e preenchi o método Index do Controller CarrinhoCompra

--- LanchesMac/LanchesMac/Controllers/CarrinhoCompraController.cs
+++ LanchesMac/LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -1,5 +1,6 @@
 ﻿using LanchesMac.Models;
 using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -17,7 +18,16 @@ namespace LanchesMac.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhCompraVM);
         }
     }
 }
