f8d6d938318ec193840a95092f1a752c002f0c08
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-10-21 19:36:28 -0300

    ✨ feat: altereir o style da barra de pesquisa e adicionei a chamada para a ViewComponent

--- LanchesMac/LanchesMac/Components/CarrinhoCompraResumo.cs
+++ LanchesMac/LanchesMac/Components/CarrinhoCompraResumo.cs
@@ -15,7 +15,13 @@ namespace LanchesMac.Components
 
         public IViewComponentResult Invoke()
         {
-            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            var itens = new List<CarrinhoCompraItem>()
+            {
+                new CarrinhoCompraItem(),
+                new CarrinhoCompraItem()
+            };
             _carrinhoCompra.CarrinhoCompraItems = itens;
 
             var carrinhCompraVM = new CarrinhoCompraViewModel
